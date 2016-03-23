4f30c20b84874b1a1f1d0340b2d97d781a80cc05
lzh <lzh@lzh.local>
2016-03-23 22:13:10 +0800

    add message that news send

--- app/models/wechat_reply_class/w_event.rb
+++ app/models/wechat_reply_class/w_event.rb
@@ -20,28 +20,38 @@ class WEvent
     end
 
     def subscribe
-	sangna_config = SangnaConfig.find_by_appid(@appid)
-	customer = CustomerList.find_or_initialize_by(openid:@weixin_message.FromUserName)
-	customer.del = 1
-	customer.updatetime = Time.now.to_i
-	customer.sangna_config_id = sangna_config.id
-	customer.subid = sangna_config.shop_id
-	customer.save
-	reply_text_message 'success'
+	   sangna_config = SangnaConfig.find_by_appid(@appid)
+	   customer = CustomerList.find_or_initialize_by(openid:@weixin_message.FromUserName)
+	   customer.del = 1
+	   customer.updatetime = Time.now.to_i
+	   customer.sangna_config_id = sangna_config.id
+	   customer.subid = sangna_config.shop_id
+	   customer.save
+       if @weixin_message.EventKey
+            scan
+       else
+            reply_news_message(generate_article('欢迎关注','xxxxx','http://callback.mijiclub.com/images/subscribe.png','http://mijiclub.com/'))
+       end
+	   
     end
 
     def unsubscribe
-	sangna_config = SangnaConfig.find_by_appid(@appid)
-	customer = CustomerList.find_or_initialize_by(openid:@weixin_message.FromUserName)
-	customer.del = 2
-	customer.updatetime = Time.now.to_i
-	customer.sangna_config_id = sangna_config.id
-	customer.subid = sangna_config.shop_id
-	customer.save
-	reply_text_message 'success'
+	   sangna_config = SangnaConfig.find_by_appid(@appid)
+	   customer = CustomerList.find_or_initialize_by(openid:@weixin_message.FromUserName)
+	   customer.del = 2
+	   customer.updatetime = Time.now.to_i
+	   customer.sangna_config_id = sangna_config.id
+	   customer.subid = sangna_config.shop_id
+	   customer.save
+	   reply_text_message 'success'
     end
 
     def scan
+        str = @weixin_message.EventKey.delete('qrscene_')
+        arr = []
+        arr << generate_article('欢迎关注','xxxxx','http://callback.mijiclub.com/images/subscribe.png','http://mijiclub.com/')
+        arr << generate_article('第二条','aaaaaaa','http://callback.mijiclub.com/images/Meinv.png','http://callback.mijiclub.com/api/third_party/home')
+        reply_news_message(arr)
     end
 
     def common_handle
