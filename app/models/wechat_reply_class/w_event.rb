98ca87f364a55b7846d2347171ba951ab9accb3f
lzh <lzh@lzh.local>
2016-03-24 15:58:34 +0800

    add base64 encode and decode

--- app/models/wechat_reply_class/w_event.rb
+++ app/models/wechat_reply_class/w_event.rb
@@ -3,7 +3,7 @@ class WEvent
 	include ReplyWeixinMessage
 	def initialize(hash,appid)
       @weixin_message = Message.factory hash
-	@appid = appid
+      @sangna_config = SangnaConfig.find_by_appid(appid)
     end
 
     def handle
@@ -20,12 +20,12 @@ class WEvent
     end
 
     def subscribe
-	   sangna_config = SangnaConfig.find_by_appid(@appid)
+	   
 	   customer = CustomerList.find_or_initialize_by(openid:@weixin_message.FromUserName)
 	   customer.del = 1
 	   customer.updatetime = Time.now.to_i
-	   customer.sangna_config_id = sangna_config.id
-	   customer.subid = sangna_config.shop_id
+	   customer.sangna_config_id = @sangna_config.id
+	   customer.subid = @sangna_config.shop_id
 	   customer.save
        if @weixin_message.EventKey.present?
             scan
@@ -36,12 +36,11 @@ class WEvent
     end
 
     def unsubscribe
-	   sangna_config = SangnaConfig.find_by_appid(@appid)
 	   customer = CustomerList.find_or_initialize_by(openid:@weixin_message.FromUserName)
 	   customer.del = 2
 	   customer.updatetime = Time.now.to_i
-	   customer.sangna_config_id = sangna_config.id
-	   customer.subid = sangna_config.shop_id
+	   customer.sangna_config_id = @sangna_config.id
+	   customer.subid = @sangna_config.shop_id
 	   customer.save
 	   reply_text_message 'success'
     end
@@ -49,10 +48,12 @@ class WEvent
     def scan
         str = @weixin_message.EventKey.delete('qrscene_')
         arr = []
-        arr << generate_article('我是第一条图文消息，请点击我','xxxxx','http://callback.mijiclub.com/images/subscribe.png','http://mijiclub.com/')
-        arr << generate_article('我是第二条图文消息，请点击我','aaaaaaa','http://callback.mijiclub.com/images/Meinv.png','http://callback.mijiclub.com/api/third_party/home')
-        arr << generate_article('我是第三条图文消息，请点击我','aaaaaaa','http://callback.mijiclub.com/images/Meinv.png','http://callback.mijiclub.com/api/third_party/home')
-        arr << generate_article('我是第四条图文消息，请点击我','aaaaaaa','http://callback.mijiclub.com/images/Meinv.png','http://callback.mijiclub.com/api/third_party/home')
+        time = Time.now.to_i
+        url = "http://mijiclub.com/weixin/page/technicianList.php?tm=#{time}&tkey=#{encodeAuth(time)}&sid=#{encodeAuth(@sangna_config.shop_id)}&openid=#{@weixin_message.FromUserName}"
+        arr << generate_article('我是第一条图文消息，请点击我','xxxxx','http://callback.mijiclub.com/images/subscribe.png',url)
+        arr << generate_article('我是第二条图文消息，请点击我','aaaaaaa','http://callback.mijiclub.com/images/Meinv.png',url)
+        arr << generate_article('我是第三条图文消息，请点击我','aaaaaaa','http://callback.mijiclub.com/images/Meinv.png',url)
+        arr << generate_article('我是第四条图文消息，请点击我','aaaaaaa','http://callback.mijiclub.com/images/Meinv.png',url)
         reply_news_message(arr)
     end
 
