2bd1ba1bcc2e2848dc966faea28f1a3dbc954ff2
lczxhf <lczxhf@sina.com>
2016-04-23 18:21:20 +0800

    ok

--- app/controllers/api/third_party_controller.rb
+++ app/controllers/api/third_party_controller.rb
@@ -48,7 +48,8 @@ class Api::ThirdPartyController < ApplicationController
 			url="https://api.weixin.qq.com/sns/oauth2/component/access_token?appid=#{params[:appid]}&code=#{params[:code]}&grant_type=authorization_code&component_appid=#{APPID}&component_access_token="+ThirdParty.get_access_token
 			result=JSON.parse(ThirdParty.get_to_wechat(url))
 			if result["openid"]
-				redirect_to "http://mijiclub.com/weixin/page/myCoupons.php?openid=#{result['openid']}&from=weixin"
+				mybase64 = Mybase64.new
+				redirect_to "http://mijiclub.com/weixin/page/myCoupons.php?sid=#{mybase64.encodeAuth(SangnaConfig.where(appid:result[:appid]).first.shop_id)}&openid=#{result['openid']}&from=weixin"
 			end
 		end		
 	end
